674c445c75bdfabbea8999b8ce3f7c3b51dde8c7
JORGE GARCIA GUTIERREZ <jorgarcia@us.es>
2021-02-11 17:21:18 +0100

    Naming changes

--- src/basic/wrapper_net.py
+++ src/basic/wrapper_net.py
@@ -1,10 +1,11 @@
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
+import torch
 
- 
+
 class WrapperNet(nn.Module):
-    def __init__(self, device, epochs, batch_size):
+    def __init__(self, device):
         super(WrapperNet, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
@@ -12,13 +13,12 @@ class WrapperNet(nn.Module):
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
-        
+
         self.criterion = nn.CrossEntropyLoss()
         self.optimizer = optim.SGD(self.parameters(), lr=0.001, momentum=0.9)
         self.device = device
-        self.epochs = epochs
-        self.batch = batch_size
-        
+        self.to(device)
+
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
@@ -27,34 +27,40 @@ class WrapperNet(nn.Module):
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x
-    
-    def train(self, train_loader):
+
+    def train(self, train_loader, epochs):
         result = []
-        
-        for epoch in self.epochs:  # loop over the dataset multiple times
-            running_loss = 0.0
-            for i, data in enumerate(train_loader, 0):
-                # get the inputs; data is a list of [inputs, labels]
-                
-                inputs, labels = data
-                inputs.to(self.device)
-                labels.to(self.device)
-                
+        for i, data in enumerate(train_loader, 0):
+            for e in range(epochs):  # loop over the dataset multiple times
+                running_loss = 0.0
+
+                inputs, labels = data[0].to(self.device), data[1].to(self.device)
+
                 # zero the parameter gradients
                 self.optimizer.zero_grad()
-    
+
                 # forward + backward + optimize
                 outputs = self(inputs)
                 loss = self.criterion(outputs, labels)
                 loss.backward()
                 self.optimizer.step()
-    
+
                 # print statistics
                 running_loss += loss.item()
-                if i % 2000 == 1999:    # print every 2000 mini-batches
-                    result.append('[%d, %5d] loss: %.3f' %
-                          (epoch + 1, i + 1, running_loss / 2000))
-                    running_loss = 0.0
+            result.append(running_loss)
+            running_loss = 0.0
         return result
 
-    
+    def test(self, test_loader):
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for i, data in enumerate(test_loader, 0):
+                print(data[0], data[1])
+                images, labels = data[0].to(self.device), data[1]#.to(self.device)
+                outputs = self(images)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+
+        return 100 * correct / total
--- src/samwrappernet_test.py
+++ src/samwrappernet_test.py
@@ -1,40 +1,39 @@
+import torch
+import torch.cuda as cuda
 import torchvision
 import torchvision.transforms as transforms
-import wrapper_net
-import visualizer
 from torch.utils.data.dataloader import DataLoader
-import torch.cuda as cuda
-import torch
- 
 
+from graphical import visualizer
+from sam import sam_wrapper_net
 
-if __name__=='__main__':
-    
+import timm
+from pprint import pprint
+
+NUM_EPOCHS = 100
+
+if __name__ == '__main__':
     dev = torch.device("cuda:0" if cuda.is_available() else "cpu")
-    
+
     TRANSFORM = transforms.Compose(
         [transforms.ToTensor(),
          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    
-    
+
     TRAIN_SET = torchvision.datasets.CIFAR10(root='../data', train=True,
-                                            download=True, transform=TRANSFORM)
+                                             download=True, transform=TRANSFORM)
     TRAIN_LOADER = DataLoader(TRAIN_SET, batch_size=4,
-                                              shuffle=True, num_workers=2)
-    
+                              shuffle=True, num_workers=2)
+
     TEST_SET = torchvision.datasets.CIFAR10(root='../data', train=False,
-                                           download=True, transform=TRANSFORM)
+                                            download=True, transform=TRANSFORM)
     TEST_LOADER = DataLoader(TEST_SET, batch_size=4,
-                                             shuffle=False, num_workers=2)
-    
+                             shuffle=False, num_workers=2)
+
     CLASSES = ('plane', 'car', 'bird', 'cat',
                'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-    
-    
-    wrapper_net = wrapper_net.WrapperNet(dev)
-    
-    #Si quito esto me funciona porque lo ejecuta en la cpu
-    #wrapper_net.to(dev)
-    
+
+    m = timm.create_model('mobilenetv3_large_100', pretrained=True)
+
+    wrapper_net = sam_wrapper_net.SAMWrapperNet(dev, model)
+
     visualizer.show_iterable(wrapper_net.train(TRAIN_LOADER))
-    
